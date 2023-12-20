581fcd7c68b691fee8207bb46ca81e7680bbdc84
GunnySingh <65249477+GunnySingh@users.noreply.github.com>
2023-12-20 17:11:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,41 +42,41 @@ with st.sidebar:
 
 
 # @st.cache_data()
-# def data_load():
-#     sim_1 = pickle.load(open('sim_1.pkl', 'rb'))
-#     sim_2 = pickle.load(open('sim_2.pkl', 'rb'))
-#     sim_3 = pickle.load(open('sim_3.pkl', 'rb'))
-#     sim_4 = pickle.load(open('sim_4.pkl', 'rb'))
-#     sim_5 = pickle.load(open('sim_5.pkl', 'rb'))
-#     sim_6 = pickle.load(open('sim_6.pkl', 'rb'))
-#     sim_7 = pickle.load(open('sim_7.pkl', 'rb'))
-#     sim_8 = pickle.load(open('sim_8.pkl', 'rb'))
-#     sim_9 = pickle.load(open('sim_9.pkl', 'rb'))
-#     sim_10 = pickle.load(open('sim_10.pkl', 'rb'))
-#     sim_11 = pickle.load(open('sim_11.pkl', 'rb'))
-#     sim_12 = pickle.load(open('sim_12.pkl', 'rb'))
-#     final = pickle.load(open('df_final.pkl', 'rb'))
-#     actor = pickle.load(open('df_actor.pkl', 'rb'))
-#     eda_df = pickle.load(open('eda_df.pkl', 'rb'))
+def data_load():
+    sim_1 = pickle.load(open('sim_1.pkl', 'rb'))
+    sim_2 = pickle.load(open('sim_2.pkl', 'rb'))
+    sim_3 = pickle.load(open('sim_3.pkl', 'rb'))
+    sim_4 = pickle.load(open('sim_4.pkl', 'rb'))
+    sim_5 = pickle.load(open('sim_5.pkl', 'rb'))
+    sim_6 = pickle.load(open('sim_6.pkl', 'rb'))
+    sim_7 = pickle.load(open('sim_7.pkl', 'rb'))
+    sim_8 = pickle.load(open('sim_8.pkl', 'rb'))
+    sim_9 = pickle.load(open('sim_9.pkl', 'rb'))
+    sim_10 = pickle.load(open('sim_10.pkl', 'rb'))
+    sim_11 = pickle.load(open('sim_11.pkl', 'rb'))
+    sim_12 = pickle.load(open('sim_12.pkl', 'rb'))
+    final = pickle.load(open('df_final.pkl', 'rb'))
+    actor = pickle.load(open('df_actor.pkl', 'rb'))
+    eda_df = pickle.load(open('eda_df.pkl', 'rb'))
 
-#     sim_mat = np.concatenate([sim_1, sim_2, sim_3, sim_4, sim_5, sim_6, sim_7, sim_8, sim_9, sim_10, sim_11, sim_12])
+    sim_mat = np.concatenate([sim_1, sim_2, sim_3, sim_4, sim_5, sim_6, sim_7, sim_8, sim_9, sim_10, sim_11, sim_12])
 
-#     return sim_mat, final, actor, eda_df
-
-
-# sim_mat, final, actor, eda_df = data_load()
+    return sim_mat, final, actor, eda_df
 
 
-# def recommend(movie):
-#     index = final[final.key == movie].index[0]
-#     results = sorted(list(enumerate(sim_mat[index])),key=lambda x :x[1])
-
-#     idx = []
-#     for i in results[:13]:
-#         idx.append(i[0])
+sim_mat, final, actor, eda_df = data_load()
 
 
-#     return final.iloc[idx]['title'].values[1:],'https://image.tmdb.org/t/p/w500'+final.iloc[idx]['poster'].values[1:],final.iloc[idx]['key'].values[1:]
+def recommend(movie):
+    index = final[final.key == movie].index[0]
+    results = sorted(list(enumerate(sim_mat[index])),key=lambda x :x[1])
+
+    idx = []
+    for i in results[:13]:
+        idx.append(i[0])
+
+
+    return final.iloc[idx]['title'].values[1:],'https://image.tmdb.org/t/p/w500'+final.iloc[idx]['poster'].values[1:],final.iloc[idx]['key'].values[1:]
 
 
 # if sel == 'Home':
